6d4dd9617f3b0eaeb8e5cd1798ed1eaee97553a6
maldo <maldo@LAPTOP-PPPO3N3Q>
2023-05-22 09:29:16 -0600

    Eliminar finalizado.

--- Eliminar.aspx
+++ Eliminar.aspx
@@ -1,4 +1,10 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Eliminar.aspx.cs" Inherits="Examen_Parcial_II.Eliminar" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h2>Eliminar Serpiente</h2>
+    <p>&nbsp;&nbsp;&nbsp; Seleccionar el animal que desea eliminar&nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList1" runat="server" Width="123px">
+        </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" Text="Eliminar" class ="btn btn-primary" OnClick="Button1_Click"/>
+    </p>
 </asp:Content>
--- Eliminar.aspx.cs
+++ Eliminar.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,44 @@ namespace Examen_Parcial_II
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                List<Serpientes> s = new List<Serpientes>();
 
+                s = Leer();
+
+                DropDownList1.DataSource = null;
+                DropDownList1.DataValueField = "NombreP";
+                DropDownList1.DataTextField = "NombreP";
+                DropDownList1.DataSource = s;
+                DropDownList1.DataBind();
+            }
+        }
+
+        private List<Serpientes> Leer()
+        {
+            List<Serpientes> lista = new List<Serpientes>();
+            string archivo = Server.MapPath("Animal.json");
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            lista = JsonConvert.DeserializeObject<List<Serpientes>>(json);
+
+            return lista;
+        }
+
+        private void Grabar(List<Serpientes> e)
+        {
+            string json = JsonConvert.SerializeObject(e);
+            string archivo = Server.MapPath("Animal.json");
+            System.IO.File.WriteAllText(archivo, json);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            List<Serpientes> s = new List<Serpientes>(); s = Leer(); int b = DropDownList1.SelectedIndex;
+
+            s.RemoveAt(b); Grabar(s); Response.Write("<script>alert('Animal eliminado exitosamente')</script>");
         }
     }
 }
